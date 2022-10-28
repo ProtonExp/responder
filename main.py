81864891a551682a49ab27fd6e2753d1883dd4ec
Tristan Cremer <116879546+ProtonExp@users.noreply.github.com>
2022-10-28 12:04:46 +0000

    0.1.2

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ usercmd = False
 print('Hi, %s.' % name)
 print('This is what the program has to say about you:')
 print(' ')
+if name == ('Emily'):
+  print('Smart and kind')
+if name == ('Zizi'):
+  print('Bear')
+if name == ('Owenkosi'):
+  print('Always touching others chromebooks')
+if name == ('Michaela'):
+  print('Toxicity killed... well I dont know')
+if name == ('Londani'):
+  print('Soccer... laughing... soccer...')
+if name == ('Angelica'):
+  ('Smart, kind and always taking notes!')
 if name == ('Kabelo'):
   print('A boy with an entire ecosystem living in his hair')
 if name == ('Luc'):
   print('A kind hearted and quiet boy')
-if name == ('Layla'):
+if name == ('Layla'): #Edited for safety reasons
   print('Her favourite saying is: I will fight you!')
 if name == ('Tristan Cremer'):
   print('Your the guy who coded this :)')
@@ -29,6 +41,13 @@ if name == ('1960') :
     print('Tristan Cremer')
     print("Mpumelelo")
     print('Layla')
+    print('Londani')
+    print('Angelica')
+    print('Luc')
+    print('Michaela')
+    print('Owenkosi')
+    print('Zizi')
+    print('Emily')
   if cmdmenu == ('0') :
     print('Run a command')
     cmdmenu = input('')
