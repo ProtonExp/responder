42cc40af4b154cd37383df5d7daa20ca80ddcb73
Tristan Cremer <116879546+ProtonExp@users.noreply.github.com>
2022-10-28 11:26:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+name = input('What is your name or code?\n')
+dev = False
+dev2 = True
+usercmd = False
+
+
+print('Hi, %s.' % name)
+print('This is what the program has to say about you:')
+print(' ')
+if name == ('Layla'):
+  print('Her favourite saying is: I will fight you!')
+if name == ('Tristan Cremer'):
+  print('Your the guy who coded this :)')
+if name == ('Mpumelelo'):
+  print('A guy who likes pixel starships')
+if name == ('1960') :
+  print('Devoper Commands Loaded')
+  print('You can now run developer commands')
+  print('You can run x different commands')
+  
+  cmdmenu = input('Command id\n')
+  if cmdmenu == ('1') :
+    print(' ')
+    print('Here are the registered users:')
+    print('Tristan Cremer')
+    print("Mpumelelo")
+    print('Layla')
+  if cmdmenu == ('0') :
+    print('Run a command')
+    cmdmenu = input('')
+    
+
+
+
+
+#Command List
+#1 = Namelist command; Lists names of all users
+#2 = View user message command; Self explanatory
